f105eba081942e5be14c17708d216cf0ee927dfe
bewpage <b.jedlinski@bewpage.com>
2017-11-09 16:41:47 +0100

    add DELETE todos by ID route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const db = {
     localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://todo-api:todoapi-2017@ds251435.mlab.com:51435/todo-api'
+    // mlab: 'mongodb://todo-api:todoapi-2017@ds251435.mlab.com:51435/todo-api'
 };
 
 mongoose.Promise = global.Promise;
