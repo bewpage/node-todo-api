3a32f8fa036c7b506abb640dbacfcbfc7caad666
bewpage <b.jedlinski@bewpage.com>
2017-11-10 14:54:13 +0100

    setup separate test and developmnet envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const db = {
-    localhost: 'mongodb://localhost:27017/TodoApp',
-    // mlab: 'mongodb://todo-api:todoapi-2017@ds251435.mlab.com:51435/todo-api'
-};
+// const db = {
+//     localhost: 'mongodb://localhost:27017/TodoApp',
+//     // mlab: 'mongodb://todo-api:todoapi-2017@ds251435.mlab.com:51435/todo-api'
+// };
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || db.localhost);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
