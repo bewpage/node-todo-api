5ec6c2f4d56a0b00e3140279b8088b2c5b9fbe4b
bewpage <b.jedlinski@bewpage.com>
2017-11-10 13:16:05 +0100

    Add PAtch todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectId} = require('mongodb');
@@ -68,6 +69,35 @@ app.delete('/todos/:id', (req, res) => {
 
 });
 
+
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    //create object of things that user can update
+    const body = _.pick(req.body, ['text', 'completed']);
+
+    //validation the ID
+    if(!ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
+});
 
 
 app.listen(port, () => {
