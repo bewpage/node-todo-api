571fe6a0ac2f93cc56810720d7b7f6a1fe47af06
bewpage <b.jedlinski@bewpage.com>
2017-11-14 14:07:10 +0100

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,18 +1,34 @@
 const {SHA256} = require('crypto-js'); //this is only for test
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const password = '123abc!';
+
+// bcrypt.genSalt(10, (err, salt) =>{
+//     bcrypt.hash(password, salt, (err, hash) =>{
+//         console.log(hash);
+//     });
+// });
+
+
+const hashedPassword = '$2a$10$6UJQZFDysmmNvpdcFDjbv.wcgWQ7NbaOrTPgQRTRJgJXjNh4/ivZ2';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
 
 
 //****** version for json web token
-const data = {
-    id: 10
-};
-
-const token = jwt.sign(data, '123abc');
-console.log(`Token: ${token}`);
-
-const decoded = jwt.verify(token, '123abc');
-console.log(`decoded: ${JSON.stringify(decoded)}`);
-console.log('decoded', decoded);
+// const data = {
+//     id: 10
+// };
+//
+// const token = jwt.sign(data, '123abc');
+// console.log(`Token: ${token}`);
+//
+// const decoded = jwt.verify(token, '123abc');
+// console.log(`decoded: ${JSON.stringify(decoded)}`);
+// console.log('decoded', decoded);
 
 
 
