35316ddd4ca4164b0c0978eb26db816e3a42ff6e
bewpage <b.jedlinski@bewpage.com>
2017-11-17 18:00:05 +0100

    added CORS headers to server.js

--- server/server.js
+++ server/server.js
@@ -16,6 +16,24 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
+//****************
+
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static(__dirname + '/../'));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE')
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'index.html'));
+});
+
+//*****************
+
 app.post('/todos', authenticate, (req, res) => {
     let todo = new Todo({
         text: req.body.text,
