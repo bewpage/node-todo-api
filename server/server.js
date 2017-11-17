3e9b781c28224a5b2e4176f0ce2efba07dcc800b
bewpage <b.jedlinski@bewpage.com>
2017-11-17 18:14:59 +0100

    update server.js

--- server/server.js
+++ server/server.js
@@ -23,14 +23,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE')
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE');
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname+'index.html'));
-});
+
+// app.use(function(req, res, next) {
+//     res.setHeader(‘Access-Control-Allow-Origin’, ‘*’);
+//     res.setHeader(‘Access-Control-Allow-Credentials’, ‘true’);
+//     res.setHeader(‘Access-Control-Allow-Methods’, ‘GET,HEAD,OPTIONS,POST,PUT,DELETE’);
+//     res.setHeader(‘Access-Control-Allow-Headers’, ‘Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers’);
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname+'index.html'));
+// });
 
 //*****************
 
